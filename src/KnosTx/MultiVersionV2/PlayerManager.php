efcd22b25202b1a2d6235793693c30056a0b2f33
KnosTx <135945845+KnosTx@users.noreply.github.com>
2024-12-04 19:45:44 +0700

    Update PlayerManager.php

--- src/KnosTx/MultiVersionV2/PlayerManager.php
+++ src/KnosTx/MultiVersionV2/PlayerManager.php
@@ -10,9 +10,12 @@ use pocketmine\player\Player;
  * Handles player-specific interactions.
  */
 class PlayerManager{
+
+    private ProtocolHandler $protocolHandler;
     private ConfigLoader $configLoader;
 
-    public function __construct(ConfigLoader $configLoader){
+    public function __construct(ProtocolHandler $protocolHandler, ConfigLoader $configLoader){
+        $this->protocolHandler = $protocolHandler;
         $this->configLoader = $configLoader;
     }
 
@@ -21,4 +24,7 @@ class PlayerManager{
             $player->sendMessage("Welcome to the server, {$player->getName()}!");
         }
     }
+
+    public function getProtocolHandler() : ProtocolHandler{
+        return $this->protocolHandler;
 }
